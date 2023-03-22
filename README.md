9b03742bfce011ce403d290595cb1d301ea03dd3
Bickey Jaisawal <jvickram2@gmail.com>
2023-03-23 00:38:28 +0530

    App running with dependencies setup with working webcam

--- README.md
+++ README.md
@@ -68,3 +68,16 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### TODOS
+dependencies installation
+
+
+
+### Instalations steps
+
+npm i @tensorflow-models/face-landmarks-detection
+
+npm install @tensorflow/tfjs
+
+pm install react-webcam  
--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import * as tf from '@tensorflow/tfjs'
+import * as facemesh from '@tensorflow-models/face-landmarks-detection'
+
+import Webcam from 'react-webcam';
+import { useRef } from 'react';
 
 function App() {
+  console.log(tf);
+
+  // Setup references
+  const webcamRef = useRef(null)
+  const canvaRef = useRef(null)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='App-header'>
+        <Webcam
+          ref={webcamRef}
+          style={
+            {
+              position: 'absolute',
+              margin: 'auto',
+              marginRight: 'auto',
+              left: 0,
+              right: 0,
+              textAlign: 'center',
+              zIndex: 9,
+              width: 640,
+              height: 480
+            }
+          }
+        />
+        <canvas
+          ref={canvaRef}
+          style={
+            {
+              position: 'absolute',
+              margin: 'auto',
+              marginRight: 'auto',
+              left: 0,
+              right: 0,
+              textAlign: 'center',
+              zIndex: 9,
+              width: 640,
+              height: 480,
+            }
+          }
+        />
       </header>
     </div>
   );
